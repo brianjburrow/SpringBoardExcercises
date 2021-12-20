2229d124cf877fd566722f821f77b7742a637f89
Brian <brianjburrow@gmail.com>
2021-12-20 12:58:41 -0500

    finished 22.3

---
+++ module_22/22.3/queries.sql
@@ -0,0 +1,25 @@
+SELECT *
+FROM owners 
+FULL JOIN vehicles
+ON owners.id = owner_id;
+
+
+select first_name, last_name, sq.count
+FROM owners JOIN (select owner_id, COUNT(*) as count
+FROM owners 
+FULL JOIN vehicles
+ON owners.id = owner_id
+GROUP BY owner_id) sq
+ON owners.id=sq.owner_id
+order by first_name;
+
+select first_name, last_name, sq.num, ROUND(sq.avgprice)
+FROM owners JOIN (select owner_id, COUNT(*) as num,
+AVG(price) as avgprice
+FROM owners 
+FULL JOIN vehicles
+ON owners.id = owner_id
+GROUP BY owner_id
+HAVING COUNT(*) > 1) sq
+ON owners.id=sq.owner_id
+order by first_name DESC;
