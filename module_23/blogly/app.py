c0b49a6dd419bce09de3ffd4af6e4ebde60d96be
Brian <brianjburrow@gmail.com>
2022-01-07 10:01:36 -0700

    implemented routes for users and new_user, extended jinja template to display pages for them

--- module_23/blogly/app.py
+++ module_23/blogly/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def render_homepage():
     return render_template('base.html')
+
+@app.route('/users')
+def render_users():
+    return render_template('users.html')
+
+@app.route('/users/new')
+def render_new_user():
+    return render_template('new_user.html')
