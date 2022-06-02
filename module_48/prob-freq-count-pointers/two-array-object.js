4b72c23b2fbfeafc09fd945f7d55e4597a89fc14
Brian <brianjburrow@gmail.com>
2022-06-02 12:36:36 -0600

    finished 48

---
+++ module_48/prob-freq-count-pointers/two-array-object.js
@@ -0,0 +1,8 @@
+function twoArrayObject(keys, values) {
+  return keys.reduce((obj, cur, idx) => {
+    obj[cur] = idx < values.length ? values[idx] : null;
+    return obj;
+  }, {});
+}
+
+module.exports = {twoArrayObject}
