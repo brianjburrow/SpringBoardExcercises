d79e55fe2ed8fb727959e6fb6955e0403d066360
Brian <brianjburrow@gmail.com>
2022-06-03 10:27:08 -0600

    completed ds

--- odule_47/dsa-sorting/bubble.js
+++ odule_47/dsa-sorting/bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function swap (array, i, j){
+    [array[i], array[j]] = [array[j], array[i]];
+}
+function bubbleSort(array) {
+    for (let i = array.length; i > 0; i--){
+        for (let j = 0; j < i - 1; j++){
+            if (array[j] > array[j+1]){
+                swap(array, j, j+1);
+            }
+        }
+    }
+    return array;
+}
 
 module.exports = bubbleSort;
