d226481a346687222896d680655333346601db19
Brian <brianjburrow@gmail.com>
2022-06-26 07:39:27 -0600

    updating resume

--- module_43/jobly/frontend/src/App.js
+++ module_43/jobly/frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter } from "react-router-dom";
 import useLocalStorage from "./hooks/useLocalStorage";
 import Navigation from "./routes/Navigation";
 import Routes from "./routes/Routes";
@@ -36,25 +36,25 @@ function App() {
     "infoLoaded=", infoLoaded,
     "currentUser=", currentUser,
     "token=", token,
-);
+  );
 
 
   // on initial load, pull user info from API.  Until a user is logged in
   // and they have a token, this shouldn't run.  
   // it only needs to rerun when a user logs out
-  useEffect(function loadUserInfo(){
+  useEffect(function loadUserInfo() {
     console.debug("App useEffect loadUserInfo", "token=", token);
-    async function getCurrentUser(){
-      if(token){
+    async function getCurrentUser() {
+      if (token) {
         // current user is logged in
-        try{
-          let {username} = jwt.decode(token);
+        try {
+          let { username } = jwt.decode(token);
           JoblyApi.token = token;
           let currentUser = await JoblyApi.getCurrentUser(username);
           setCurrentUser(currentUser);
           // get the list of jobs they have applied for
           setApplicationIds(new Set(currentUser.applications));
-        } catch (err){
+        } catch (err) {
           console.error("App loadUserInfo: did not load correctly", err);
           setCurrentUser(null);
         }
@@ -68,44 +68,44 @@ function App() {
     console.log("GCU", getCurrentUser)
     getCurrentUser();
   }, [token]);
-  
+
   // handle logouts
-  function logout(){
+  function logout() {
     setCurrentUser(null);
     setToken(null);
   }
 
   // Check if the current user has applied for a specific job
-  function hasAppliedToJob(id){
+  function hasAppliedToJob(id) {
     return applicationIds.has(id);
   }
   // handle signup
   // auto log in
 
-  async function signup(signupData){
-    try{
+  async function signup(signupData) {
+    try {
       let token = await JoblyApi.signup(signupData);
       setToken(token);
-      return {sucess:true};
-    } catch (err){
+      return { sucess: true };
+    } catch (err) {
       console.error("signup failed", err);
-      return {success: false, errors: err};
+      return { success: false, errors: err };
     }
   }
 
   // handle login
-  async function login(loginData){
-    try{
+  async function login(loginData) {
+    try {
       let token = await JoblyApi.login(loginData);
       setToken(token);
-      return {success: true};
-    } catch(err){
-      return {success: false, errors: err};
+      return { success: true };
+    } catch (err) {
+      return { success: false, errors: err };
     }
   }
 
   // apply to a job: no return so no async needed
-  function applyToJob(id){  
+  function applyToJob(id) {
     if (hasAppliedToJob(id)) return;
     JoblyApi.applyToJob(currentUser.username, id);
     setApplicationIds(new Set([...applicationIds, id]));
@@ -114,11 +114,11 @@ function App() {
   if (!infoLoaded) return <LoadingSpinner />;
   return (
     <BrowserRouter>
-      <UserContext.Provider 
-      value={{ currentUser, setCurrentUser, hasAppliedToJob, applyToJob }}>
+      <UserContext.Provider
+        value={{ currentUser, setCurrentUser, hasAppliedToJob, applyToJob }}>
         <div className="App">
-          <Navigation logout={logout}/>
-          <Routes login={login} signup={signup}/>
+          <Navigation logout={logout} />
+          <Routes login={login} signup={signup} />
         </div>
       </UserContext.Provider>
     </BrowserRouter>
